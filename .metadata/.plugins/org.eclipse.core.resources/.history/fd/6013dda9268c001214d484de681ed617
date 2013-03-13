1bd84958775c7e92842fdc680a75f172ea620c01
jburns20 <j@burnsfamily.info>
2013-03-13 14:41:37 -0700

    removed global variables

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/fd/6013dda9268c001214d484de681ed617
@@ -0,0 +1,6 @@
+package model;
+
+public class Rules {
+	public static final int DIMENSION = 3;
+	public static final int LEVELS = 2;
+}
--- Tic Tac Toe/src/model/Rules.java
+++ Tic Tac Toe/src/model/Rules.java
@@ -1,6 +1,4 @@
 package model;
 
 public class Rules {
-	public static final int DIMENSION = 3;
-	public static final int LEVELS = 2;
 }
