0aacfc131a895c535dcc3f2830359bc3aa988c41
jburns20 <j@burnsfamily.info>
2013-03-18 14:47:55 -0700

    more class changes

--- Tic Tac Toe/src/view/GamePanel.java
+++ Tic Tac Toe/src/view/GamePanel.java
@@ -2,6 +2,9 @@ package view;
 
 import javax.swing.JPanel;
 
+/**
+ * The content pane for the game. Contains a ControlPanel and a GameBoardPanel.
+ */
 public class GamePanel extends JPanel {
 	private static final long serialVersionUID = 1860979716621182121L;
 	
