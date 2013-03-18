6d193922b08aedc2feac375005ac2f5d68daf90b
jburns20 <j@burnsfamily.info>
2013-03-18 15:01:06 -0700

    Comments for view classes

--- Tic Tac Toe/src/view/ControlPanel.java
+++ Tic Tac Toe/src/view/ControlPanel.java
@@ -2,6 +2,9 @@ package view;
 
 import javax.swing.JPanel;
 
+/**
+ * Includes all of the UI elements besides the game board, such as the player's score and reset / restart buttons.
+ */
 public class ControlPanel extends JPanel {
 	private static final long serialVersionUID = 4231304967321746399L;
 
--- Tic Tac Toe/src/view/TilePanel.java
+++ Tic Tac Toe/src/view/TilePanel.java
@@ -2,6 +2,9 @@ package view;
 
 import javax.swing.JPanel;
 
+/**
+ * Represents any square of the game view. Can show an image of an X or an O if necessary.
+ */
 public class TilePanel extends JPanel {
 	private static final long serialVersionUID = -1330024308079666309L;
 
