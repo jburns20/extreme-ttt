22addf54bce98ca2936b8787291cdb449f5c990c
jburns20 <j@burnsfamily.info>
2013-03-18 15:09:23 -0700

    Rules method headers

--- Tic Tac Toe/src/model/Rules.java
+++ Tic Tac Toe/src/model/Rules.java
@@ -1,4 +1,27 @@
 package model;
+import java.util.Map;
+import general.Location;
 
 public class Rules {
+	private Map<Location, Integer> updatedLocations;
+	
+	public Location getNextMoveLocation() {
+		return null;
+	}
+	
+	public int currentPlayer() {
+		return -1;
+	}
+	
+	public void makeMove(int player, Location loc) {
+		
+	}
+	
+	public Map<Location, Integer> getUpdatedLocations() {
+		return updatedLocations;
+	}
+	
+	public void clearUpdates() {
+		
+	}
 }
