866142a05d35518c79a080e550db70a6092a2483
Andrew Friedman <afriedman1@hwemail.com>
2013-03-18 15:01:50 -0700

    AI interface

--- Tic Tac Toe/src/model/AI.java
+++ Tic Tac Toe/src/model/AI.java
@@ -1,5 +1,17 @@
 package model;
 
+import general.Location;
+
+/**
+ * An interface specifying the necessary behaviors of any computer
+ * players (AIs).
+ */ 
 public interface AI {
 	
+	/**
+	 * 
+	 */ 
+	Location selectMove(GameBoard board, Location playLocation);
+	String wittyCatchphrase();
+	int getDifficultyLevel();
 }
--- Tic Tac Toe/src/model/RandomAI.java
+++ Tic Tac Toe/src/model/RandomAI.java
@@ -1,5 +1,20 @@
 package model;
 
+import general.Location;
+
 public class RandomAI implements AI {
+
+	public Location selectMove(GameBoard board, Location playLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String wittyCatchphrase() {
+		return "Beating you was sheer luck...";
+	}
+
+	public int getDifficultyLevel() {
+		return 1;
+	}
 
 }
